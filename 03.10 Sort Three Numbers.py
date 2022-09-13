c1fd02c0cbfbef98c44e6916b01e5be6f7156750
wgrevis <wgrevis@ualr.edu>
2022-09-13 23:57:10 +0000

    unit3

--- 03.10 Sort Three Numbers.py
+++ 03.10 Sort Three Numbers.py
@@ -1,3 +1,16 @@
 x = int(input("Enter First Number: "))
 y = int(input("Enter Second Number: "))
 z = int(input("Enter Third Number: "))
+if x > z:
+    x = x + z
+    z = x - z
+    x = x - z  
+if x > y:
+    x = x + y
+    y = x - y
+    x = x - y
+if y > z:
+    y = y + z
+    z = y - z
+    y = y - z
+print (x, y, z)
