ba9d6eaaf22d6093609b775b7bb55757ef51f88a
wgrevis <wgrevis@ualr.edu>
2022-09-13 22:35:41 +0000

    unit3

--- 03.04 Digits In Ascending Order.py
+++ 03.04 Digits In Ascending Order.py
@@ -1,1 +1,8 @@
-x =int(input("Enter a Number: ")
+num = int(input("Enter a number: "))
+num3 = num % 10
+num2 = num // 10 % 10
+num1 = num // 100 % 10
+if num1 + 1 == num2 and num2 + 1 == num3: 
+    print("YES")
+else: 
+    print("NO")
