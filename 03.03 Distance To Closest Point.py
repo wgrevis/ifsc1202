0b895cfbb417dbd0d8d3db4f4a6b047e4063b34a
wgrevis <wgrevis@ualr.edu>
2022-09-14 14:39:54 +0000

    unit3

--- 03.03 Distance To Closest Point.py
+++ 03.03 Distance To Closest Point.py
@@ -1,3 +1,10 @@
 a = int(input("Enter Point A: "))
 b = int(input("Enter Point B: "))
 c = int(input("Enter Point C: "))
+distance = 0
+ab= abs(a - b)
+ac= abs(a - c)
+if ab < ac :
+    print(ab)
+else :
+    print(ac)
