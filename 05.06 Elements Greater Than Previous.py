0a6732c919d38d2cb3471dd0937d93bba9f8c671
wgrevis <wgrevis@ualr.edu>
2022-09-21 20:17:06 +0000

    unit4/5

--- 05.06 Elements Greater Than Previous.py
+++ 05.06 Elements Greater Than Previous.py
@@ -1,7 +1,8 @@
 x = int(input ("Enter a Number (zero to quit): "))
 counter = 0
+prev = x - 1
 while x != 0:
-    if x > x - 1 : 
+    if x > prev: 
         counter = counter + 1
     x = int(input ("Enter a Number (zero to quit): "))
 print("Number of Values Greater Than the Previous: ",counter)
