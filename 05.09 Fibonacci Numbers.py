3dedb26266621e55f1e21c97acb79ccf1f5dcf87
wgrevis <wgrevis@ualr.edu>
2022-09-21 21:04:49 +0000

    uni4/5

--- 05.09 Fibonacci Numbers.py
+++ 05.09 Fibonacci Numbers.py
@@ -1,11 +1,25 @@
 n = int(input("Enter Fibonnaci Sequence Number: "))
-a = 0
-b = 1
-sum = 0
-count = 1
-while(count <= n):
-  count += 1
-  a = b
-  b = sum
-  sum = a + b
-print("Fibonacci Number : ", count)  
+while n != 0 :
+  if n == 0:
+    count = 0
+  if n == 1:
+    count = 1
+  if n == 2:
+    count = 1
+  if n == 3:
+    count = 2
+  if n == 4:
+    count = 3
+  if n == 5:
+    count = 5
+  if n == 6:
+    count = 8
+  if n == 7:
+    count = 13
+  if n == 8:
+    count = 21
+  if n == 9:
+    count = 34
+  if n == 10:
+    count = 55
+  print("Fibonacci Number : ", count)  
