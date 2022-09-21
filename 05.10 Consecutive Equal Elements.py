eba40de6eb3af114b7ae60df1a8471d37066485e
wgrevis <wgrevis@ualr.edu>
2022-09-21 20:30:29 +0000

    unit4/5

--- 05.10 Consecutive Equal Elements.py
+++ 05.10 Consecutive Equal Elements.py
@@ -2,8 +2,9 @@ x = int(input("Enter a Number (zero to quit): "))
 maximum = x
 counter = 0
 while x != 0 :
+    x = int(input("Enter a Number (zero to quit): "))
     if x > maximum:
         maximum = x
     if x == x :
-        times = counter + 1 
-print ("{} repeated {} times.".format(x, times))
+        counter = counter + 1 
+    print("{} repeated {} number of times.".format(maximum, counter))
